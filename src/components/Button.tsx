c06553ea1930b6472a2f0c7efebd8637e518bdd3
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-07-30 01:51:23 +0530

    release: new prop: onClick and tailwindCSS support

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
-import {ReactNode} from 'react'
+import { ReactNode } from 'react'
 
 type ButtonProps = {
-    children: ReactNode
+    children: ReactNode;
+    onClick?: () => void;
 }
 
-const Button = ({ children }: ButtonProps) => {
+const Button = ({ children, onClick }: ButtonProps) => {
     return (
-        <button>
+        <button
+            onClick={onClick}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        >
             {children}
         </button>
     )
