5a0e52d436b3a941219b325d8a2710adb2bc8061
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-07-30 01:29:55 +0530

    fix: build configuration and update package

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,34 @@
-import { defineConfig } from "rollup";
-import typescript from "@rollup/plugin-typescript";
+// import { defineConfig } from "rollup";
+// import typescript from "@rollup/plugin-typescript";
 
-export default defineConfig({
-  input: "src/index.ts",
-  output: {
-    dir: "dist",
-    format: "es",
-    name: "sayan-ui",
-  },
-  external: ["react", "react-dom"],
-  plugins: [typescript({ tsconfig: "tsconfig.json" })],
-});
+// export default defineConfig({
+//   input: "src/index.ts",
+//   output: {
+//     dir: "dist",
+//     format: "es",
+//     name: "sayan-ui",
+//   },
+//   external: ["react", "react-dom"],
+//   plugins: [typescript({ tsconfig: "tsconfig.json" })],
+// });
+
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      dir: 'dist',
+      format: 'es',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      sourcemap: true,
+    },
+  ],
+  external: ['react', 'react-dom'],
+  plugins: [
+    nodeResolve(),
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
+};
