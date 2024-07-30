86a562295434713866b1ae05bb897b4c0888fec4
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-07-30 08:35:09 +0530

    fix: className props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,22 @@
-import * as React from 'react'
-import { ReactNode } from 'react'
+import * as React from 'react';
+import { ReactNode } from 'react';
+import classNames from 'classnames';
 
 type ButtonProps = {
     children: ReactNode;
     onClick?: () => void;
     className?: string;
-}
+};
 
 const Button = ({ children, onClick, className }: ButtonProps) => {
     return (
         <button
             onClick={onClick}
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            className={classNames('bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded', className)}
         >
             {children}
         </button>
-    )
-}
+    );
+};
 
-export { Button }
+export { Button };
